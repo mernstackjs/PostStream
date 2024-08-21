f59b2510cca950f00d762c45a0a1a426060c88cc
Ahmed Abdirizaak <mernstack.js@gmail.com>
2024-08-21 15:02:10 +0200

    waxa aan saxay authrouter nagaciisa

--- backend/src/router/index.js
+++ backend/src/router/index.js
@@ -1,7 +1,10 @@
 import { Router } from "express";
+import authRouter from "./api/authRouter.js";
+import userRouter from "./api/userRouter.js";
+import { authentication } from "../../middleware/authentication.js";
 
 const router = Router();
 
-import authRouter from "./api/userRouter.js";
 router.use("/auth", authRouter);
+router.use("/auth", authentication, userRouter);
 export default router;
