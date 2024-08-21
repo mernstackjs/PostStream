9fd1199c5b34bbe929ad43864da6162daf1285e8
Ahmed Abdirizaak <mernstack.js@gmail.com>
2024-08-21 13:07:15 +0200

    waxa sameeyey usermodel

--- backend/src/model/userModel.js
+++ backend/src/model/userModel.js
@@ -22,7 +22,7 @@ userSchema.pre("save", async function (next) {
   next();
 });
 
-userSchema.methods.comparePassword = async function (password) {
+userSchema.methods.comparePasswords = async function (password) {
   try {
     return await bcrypt.compare(password, this.password);
   } catch (error) {
