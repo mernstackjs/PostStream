9cdaa454a8513eb7462870b0f71c021f6515cc59
Ahmed Abdirizaak <mernstack.js@gmail.com>
2024-08-21 12:59:26 +0200

    waxa sameeyey usermodel

---
+++ backend/src/model/userModel.js
@@ -0,0 +1,34 @@
+import mongoose, { model, Schema } from "mongoose";
+import bcrypt from "bcrypt";
+const userSchema = new Schema(
+  {
+    username: String,
+    email: String,
+    password: String,
+    posts: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Post",
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+  next();
+});
+
+userSchema.methods.comparePassword = async function (password) {
+  try {
+    return await bcrypt.compare(password, this.password);
+  } catch (error) {
+    throw new Error("Error comparing passwords");
+  }
+};
+
+const User = model("User", userSchema);
+export default User;
