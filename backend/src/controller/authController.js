da8587ac4cb7ac378a8d25f39d7f5210c9f25e1f
Ahmed Abdirizaak <mernstack.js@gmail.com>
2024-08-21 15:41:54 +0200

    waxa aan sameeye function logout ah

--- backend/src/controller/authController.js
+++ backend/src/controller/authController.js
@@ -93,3 +93,24 @@ export const Login = async (req, res) => {
     });
   }
 };
+
+export const Logout = async (req, res) => {
+  try {
+    res.clearCookie("token", {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "strict",
+    });
+    res.status(200).json({
+      success: true,
+      message: "Logout successful.",
+    });
+  } catch (error) {
+    console.error("Logout error:", error);
+    res.status(500).json({
+      success: false,
+      message: "An error occurred during logout.",
+      error: error.message,
+    });
+  }
+};
